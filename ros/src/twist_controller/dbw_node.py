5a4bacb13da345258f836264ba43b13ad575da75
Jakub Kolodziejczyk <jakub.kolodziejczyk@mail.com>
2017-09-12 16:43:30 -0700

    Reworked commands sending so that when trottle is nonzero, brake command is not send and vice versa

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -121,22 +121,30 @@ class DBWNode(object):
             rate.sleep()
 
     def publish(self, throttle, brake, steer):
+
         tcmd = ThrottleCmd()
         tcmd.enable = True
         tcmd.pedal_cmd_type = ThrottleCmd.CMD_PERCENT
         tcmd.pedal_cmd = throttle
-        self.throttle_pub.publish(tcmd)
 
         scmd = SteeringCmd()
         scmd.enable = True
         scmd.steering_wheel_angle_cmd = steer
-        self.steer_pub.publish(scmd)
 
         bcmd = BrakeCmd()
         bcmd.enable = True
         bcmd.pedal_cmd_type = BrakeCmd.CMD_TORQUE
         bcmd.pedal_cmd = brake
-        self.brake_pub.publish(bcmd)
+
+        if brake == 0:
+
+            self.throttle_pub.publish(tcmd)
+            self.steer_pub.publish(scmd)
+
+        if throttle == 0:
+
+            self.brake_pub.publish(bcmd)
+            self.steer_pub.publish(scmd)
 
     def twist_commands_cb(self, msg):
 
